1087aa23559322a389cf99972411b7ae5c6905f7
Sharmiko <sharmiko123@gmail.com>
2020-11-05 00:23:54 +0400

    add new endpoint and add product detail view

--- ecomstore/apps/catalog/urls.py
+++ ecomstore/apps/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from ecomstore.apps.catalog.views import CategoryList
+from ecomstore.apps.catalog.views import CategoryList, ProductDetail
 
 urlpatterns = [
-    path('categories/', CategoryList.as_view())
+    path('categories/', CategoryList.as_view()),
+    path('product/', ProductDetail.as_view())
 ]
 
--- ecomstore/apps/catalog/views.py
+++ ecomstore/apps/catalog/views.py
@@ -1,15 +1,18 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from ecomstore.apps.catalog.models import Category
-from ecomstore.apps.catalog.serializers import CategorySerializer
+from ecomstore.apps.catalog.models import Category, Product
+from ecomstore.apps.catalog.serializers import (
+    CategorySerializer, ProductSerializer
+)
 
 
 class CategoryList(APIView):
     """ List all categories
     """
 
-    def get(self, request, format=None) -> Response:
+    def get(self, request) -> Response:
         """ returns all unique categories
         """
         categories = Category.objects.distinct().only('name')
@@ -17,4 +20,26 @@ class CategoryList(APIView):
         data = [value for row in serializer.data for value in row.values()]
 
         return Response({'categories': data})
+
+
+class ProductDetail(APIView):
+    """ Get product details based on its uuid
+    """
+
+    def post(self, request) -> Response:
+        """ get product based on uuid
+        """
+        print(request, request.__dir__())
+        uuid = request.data.get('uuid')
+        if not uuid:
+            return Response(
+                {
+                    'message': 'Product UUID was not provided',
+                    'status': 'error'
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+        product = Product.objects.get(uuid=uuid)
+        serializer = ProductSerializer(product)
+
+        return Response({'product': serializer.data})
 
